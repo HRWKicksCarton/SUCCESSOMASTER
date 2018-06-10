d6cda71288c2dab266e298b7f1beaed8e9f807f2
Herman <hermanwagner@outlook.com>
2018-06-10 15:30:01 +0200

    some

---
+++ Successo/App_Start/BundleConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Successo
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add( new ScriptBundle("~/bundles/scripts").Include(
+                "~/Content/style.css",
+                "~/Content/bootstrap.min.css",
+                "~/Content/responsive.css",
+                "~/Content/font-awesome.min.css",
+                "~/Content/effects/set2.css",
+                "~/Content/effects/normalize.css",
+                "~/Content/effects/component.css"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Scripts/jquery.min.js",
+                      "~/Scripts/nav.js",
+                      "~/Scripts/custom.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/effects/masonry.pkgd.min.js",
+                      "~/Scripts/effects/imagesloaded.js",
+                      "~/Scripts/effects/classie.js",
+                      "~/Scripts/effects/AnimOnScroll.js",
+                      "~/Scripts/effects/modernizr.custom.js",
+                      "~/Scripts/html5shiv.js"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ Successo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Successo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Successo")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99ea3ca4-3cdb-42d1-b743-053aef4fb865")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
